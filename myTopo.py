9cd927e87a0d82a56335e71fa74cf8ccc00ce1af
DamianGanopolsky <damian98dgg@gmail.com>
2022-06-25 12:20:54 -0300

    deleted unused code

--- myTopo.py
+++ myTopo.py
@@ -1,4 +1,5 @@
 from mininet.topo import Topo
+import sys
 
 class LinearTopology(Topo):
 	"Simple topology example."
@@ -13,78 +14,26 @@ class LinearTopology(Topo):
 		upperRightHost = self.addHost( 'h2' )
 		bottomLeftHost = self.addHost( 'h3' )
 		bottomRightHost = self.addHost( 'h4' )
-		
-# 		Add switches
+
+        #Add switches
 		switches = []
-		for i in range(number_of_switches):
+		for i in range(int(number_of_switches)):
 			alias = "s"+str(i)
 			switches.append(self.addSwitch(alias))
-# 		switches = [self.addSwitch( 's1' ), self.addSwitch( 's2' ), self.addSwitch( 's3' ), self.addSwitch( 's4' )]
 
-# 		# Add links
+ 		# Add links
 		self.addLink( upperLeftHost, switches[0] )
 		self.addLink( bottomLeftHost, switches[0] )
 
-		for i in range(1,number_of_switches):
+		for i in range(1,int(number_of_switches)):
 			self.addLink( switches[i-1],switches[i])
 
-		self.addLink( switches[number_of_switches-1], upperRightHost )
-		self.addLink( switches[number_of_switches-1], bottomRightHost )
+		self.addLink( switches[int(number_of_switches)-1], upperRightHost )
+		self.addLink( switches[int(number_of_switches)-1], bottomRightHost )
 
-class Foo:
-	"Simple topology example."
-	def __init__(self, number_of_switches):
-		"Create custom topo."
-		# Initialize topology
-		
-		# Topo.__init__( self )
-		# print("Creating topo with {} switches".format(str(number_of_switches)))
+
+topos = { 'linear': LinearTopology }
 	
-		# # Add hosts 
-		# upperLeftHost = self.addHost( 'h1' )
-		# upperRightHost = self.addHost( 'h2' )
-		# bottomLeftHost = self.addHost( 'h3' )
-		# bottomRightHost = self.addHost( 'h4' )
-		print(number_of_switches)
 
-		switches = []
-		for i in range(number_of_switches):
-			alias = "s"+str(i)
-			switches.append(alias)
-
-		links = []
-
-		links.append("upperLeftHost, switches 0")
-		links.append("bottomLeftHost, switches 0")
-		# self.addLink( upperLeftHost, switches[0] )
-		# self.addLink( bottomLeftHost, switches[0] )
-		for i in range(1,number_of_switches):
-			links.append("switches"+str(i-1)+ "switches "+str(i))
-
-		
-		links.append("switches" + str(number_of_switches-1)+ ", upperRightHost")
-		links.append("switches" + str(number_of_switches-1)+ "bottomRightHost")
-		print(links)
-		# self.addLink( switches[3], upperRightHost )
-		# self.addLink( switches[3], bottomRightHost )
-
-		# # Add switches
-		# switches = [self.addSwitch( 's1' ), self.addSwitch( 's2' ), self.addSwitch( 's3' ), self.addSwitch( 's4' )]
-
-		# # Add links
-		# self.addLink( upperLeftHost, switches[0] )
-		# self.addLink( bottomLeftHost, switches[0] )
-		# self.addLink( switches[0], switches[1] )
-		# self.addLink( switches[1], switches[2] )
-		# self.addLink( switches[2], switches[3] )
-		# self.addLink( switches[3], upperRightHost )
-		# self.addLink( switches[3], bottomRightHost )
-import sys
-
-foo = Foo(int(sys.argv[1]))
-def main():
-	print(args)
-	
-	#topos = { 'linear': LinearTopology }
 
 

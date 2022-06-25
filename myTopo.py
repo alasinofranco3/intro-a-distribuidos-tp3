1a7d152a9b3d7112affb5af295b20826ef13f58e
FerFabbiano <ffabbiano@fi.uba.ar>
2022-06-25 11:19:05 -0300

    Change topology

--- myTopo.py
+++ myTopo.py
@@ -1,28 +1,32 @@
 from mininet.topo import Topo
 
-class MyTopo( Topo ):
+class LinearTopology(Topo):
 	"Simple topology example."
-	def __init__( self ):
+	def __init__(self, number_of_switches):
 		"Create custom topo."
 		# Initialize topology
 		Topo.__init__( self )
-		
+		print("Creating topo with {} switches".format(str(number_of_switches)))
+	
 		# Add hosts 
 		upperLeftHost = self.addHost( 'h1' )
 		upperRightHost = self.addHost( 'h2' )
 		bottomLeftHost = self.addHost( 'h3' )
 		bottomRightHost = self.addHost( 'h4' )
-		
+
 		# Add switches
-		leftSwitch = self.addSwitch( 's1' )
-		rightSwitch = self.addSwitch( 's2' )
+		switches = [self.addSwitch( 's1' ), self.addSwitch( 's2' ), self.addSwitch( 's3' ), self.addSwitch( 's4' )]
 
 		# Add links
-		self.addLink( upperLeftHost, leftSwitch )
-		self.addLink( bottomLeftHost, leftSwitch )
-		self.addLink( leftSwitch, rightSwitch )
-		self.addLink( rightSwitch, upperRightHost )
-		self.addLink( rightSwitch, bottomRightHost )
+		self.addLink( upperLeftHost, switches[0] )
+		self.addLink( bottomLeftHost, switches[0] )
+		self.addLink( switches[0], switches[1] )
+		self.addLink( switches[1], switches[2] )
+		self.addLink( switches[2], switches[3] )
+		self.addLink( switches[3], upperRightHost )
+		self.addLink( switches[3], bottomRightHost )
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+
+topos = { 'linear': LinearTopology }
+
 

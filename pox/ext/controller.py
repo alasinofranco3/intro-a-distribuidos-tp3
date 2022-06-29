62067da3266da75edf418cc00f9b54311e2ef648
FerFabbiano <ffabbiano@fi.uba.ar>
2022-06-29 12:08:06 -0300

    Add all rules to firewall

--- pox/ext/controller.py
+++ pox/ext/controller.py
@@ -6,7 +6,7 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
+from pox.lib.addresses import EthAddr, IPAddr
 from collections import namedtuple
 import os
 from pox.forwarding import l2_learning
@@ -27,7 +27,6 @@ class Controller(EventMixin):
     
     def _handle_ConnectionUp(self, event):
         # Add your logic here ...
-        log.debug("Handling new connection")
         log.debug("Handling Connection Up for switch {}".format(event.dpid))
         #log.debug(event)
         
@@ -36,48 +35,96 @@ class Controller(EventMixin):
 
 
     def build_firewall_rules(self, event):
-        log.debug("Start building firewall rules")
+        log.debug("Start building firewall rules for switch {}".format(event.dpid))
 
         self.build_port_80_rule(event)        
         self.build_host_1_port_5001_udp_rule(event)
         self.build_disable_host_communication_rule(event)
 
         log.debug("Finish building firewall rules")
-        pass
 
-    
+
     def build_port_80_rule(self, event):
         log.debug("Building port 80 rule")
 
+        # Build match for TCP
         match = of.ofp_match(
             dl_type=pkt.ethernet.IP_TYPE,
             nw_proto=pkt.ipv4.TCP_PROTOCOL,
             tp_dst=80
         )
 
-        msg = of.ofp_flow_mod()
-        msg.match = match
-        msg.actions.append(of.ofp_action_tp_port.set_dst(of.OFPP_NONE))
-        event.connection.send(msg)
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
 
+        # Build match for UDP
         match = of.ofp_match(
             dl_type=pkt.ethernet.IP_TYPE,
             nw_proto=pkt.ipv4.UDP_PROTOCOL,
             tp_dst=80
         )
 
-        msg = of.ofp_flow_mod()
-        msg.match = match
-        msg.actions.append(of.ofp_action_tp_port.set_dst(of.OFPP_NONE))
-        event.connection.send(msg)    
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)  
 
     
     def build_host_1_port_5001_udp_rule(self, event):
-        pass
+        log.debug("Building host 1, port 5001, protocolo UDP rule")
+
+        # Build match for rule
+        match = of.ofp_match(
+            dl_type=pkt.ethernet.IP_TYPE,
+            nw_proto=pkt.ipv4.UDP_PROTOCOL,
+            tp_dst=5001,
+            nw_src = IPAddr("10.0.0.1")
+        )
+
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
 
 
     def build_disable_host_communication_rule(self, event):
-        pass
+        log.debug("Building disable host communication between host 1 and host 4 rule")
+
+        match = of.ofp_match(
+            dl_type=pkt.ethernet.IP_TYPE,
+            nw_proto=pkt.ipv4.TCP_PROTOCOL,
+            nw_src = IPAddr("10.0.0.1"),
+            nw_dst = IPAddr("10.0.0.4"),
+        )
+
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
+
+        match = of.ofp_match(
+            dl_type=pkt.ethernet.IP_TYPE,
+            nw_proto=pkt.ipv4.UDP_PROTOCOL,
+            nw_src = IPAddr("10.0.0.1"),
+            nw_dst = IPAddr("10.0.0.4"),
+        )
+
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
+
+        match = of.ofp_match(
+            dl_type=pkt.ethernet.IP_TYPE,
+            nw_proto=pkt.ipv4.TCP_PROTOCOL,
+            nw_src = IPAddr("10.0.0.4"),
+            nw_dst = IPAddr("10.0.0.1"),
+        )
+
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
+
+        match = of.ofp_match(
+            dl_type=pkt.ethernet.IP_TYPE,
+            nw_proto=pkt.ipv4.UDP_PROTOCOL,
+            nw_src = IPAddr("10.0.0.4"),
+            nw_dst = IPAddr("10.0.0.1"),
+        )
+
+        self.drop_packet_with_match_and_send_msg_to_firewall(match, event)
+
+
+    def drop_packet_with_match_and_send_msg_to_firewall(self, match, event):
+        msg = of.ofp_flow_mod()
+        msg.match = match
+        msg.actions.append(of.ofp_action_tp_port.set_dst(of.OFPP_NONE))
+        event.connection.send(msg)
 
 
 def launch():
